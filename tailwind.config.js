dba4c03f46f36930d0def7ba86071d6e4998830f
Dzhalal1 <Dumarro0212>
2021-04-21 23:28:50 +0300

    animate шаров на странице авторизации

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,20 @@ module.exports = {
     theme: {
         extend: {
             backgroundColor: ['active'],
+            animation: {
+                bounce: 'bounce 5s infinite',
+                'bounce-before': 'bounce 3s infinite',
+            },
+            keyframes: {
+                bounce: {
+                    '0%, 100%': {
+                        transform: 'translateY(-25%)',
+                        animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+                    }
+                }
+            }
         },
-        colors:{
+        colors: {
             transparent: 'transparent',
             ksaa: '#20b2aa',
             black: colors.black,
