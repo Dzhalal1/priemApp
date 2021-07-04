f75a5e38ddc75eba84d1208b951fd02dc7c6e5c1
User <artela1992@gmail.com>
2021-07-04 21:22:02 +0300

    начал обнавление степс

---
+++ src/components/Steps.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div class="p-2">
+            <p class="text-center">{{currentStatusTitle}}</p>
+        </div>
+        <div class="flex items-center justify-between p-8">
+            <div v-for="step in stepList" :key="step.id">{{step.id}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Steps",
+        data() {
+            return {
+                stepList: [
+                    {
+                        id: 1,
+                        title: 'В процессе подачи',
+                        hide: false,
+                        icon: ''
+                    },
+                    {
+                        id: 2,
+                        title: 'Одобренно секретаром',
+                        hide: false,
+                        icon: ''
+                    },
+                    {
+                        id: 3,
+                        title: 'Ожидание вступительных испытаний',
+                        hide: false,
+                        icon: ''
+                    },
+                    {
+                        id: 4,
+                        title: 'Ожидние результатов приемной компании',
+                        hide: false,
+                        icon: ''
+                    },
+                    {
+                        id: 5,
+                        title: 'Зачислен на обучение',
+                        hide: false,
+                        icon: ''
+                    },
+                    {
+                        id: 6,
+                        title: 'Не зачислен на обучение',
+                        hide: false,
+                        icon: ''
+                    },
+                ]
+            }
+        },
+        computed: {
+          currentStatusTitle() {
+              const step = this.stepList.find(steps => steps.id === this.currentStatus.status_id)
+              if (step === undefined) {
+                  return 'Статус неопределен'
+              } else {
+                  return step.title
+              }
+          }
+        },
+        props: {
+            currentStatus: Object,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,62 +3,63 @@
         <ion-content :fullscreen="true">
             <div class="circle">
                 <ion-grid class="m-4">
-                    <div class="bcdrp-bg shadow-md text-lg  rounded-xl">
-                        <div class="flex justify-center pb-6">
-                            <img :src="require('../assets/img/WaitingMonochromatic.svg')" alt="">
-                        </div>
+                    <steps :current-status="currentStatus"></steps>
+<!--                    <div class="bcdrp-bg shadow-md text-lg  rounded-xl">-->
+<!--                        <div class="flex justify-center pb-6">-->
+<!--                            <img :src="require('../assets/img/WaitingMonochromatic.svg')" alt="">-->
+<!--                        </div>-->
 
-                        <div class="step">
-                            <ul v-if="currentStatus.status_id !== 0">
-                                <li :key="status.id" v-for="status in statuses.filter(s=> !s.hide)"
-                                    :class="{'text-ksaa': status.id <= currentStatus.status_id  && status.id < 6,'text-red-400':status.id===6,'items-center':true,'grid':true,'flex':true,'grid-cols-1':true}">
-                                    <div class="flex justify-between items-center">
-                                        <span v-if="status.id<6">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
-                                         fill="currentColor">
-                                        <path fill-rule="evenodd"
-                                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                              clip-rule="evenodd"/>
-                                    </svg>
-                                    </span>
-                                        <span v-else>
-                                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
-                                           fill="currentColor">
-  <path fill-rule="evenodd"
-        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-        clip-rule="evenodd"/>
-</svg>
-                                   </span>
-                                        <div>
-                                            {{status.title}}
-                                        </div>
-                                        <div>
-                                            <svg @click="status.show_description=!status.show_description"
-                                                 xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                                 viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                      d="M19 9l-7 7-7-7"/>
-                                            </svg>
-                                        </div>
-                                    </div>
+<!--                        <div class="step">-->
+<!--                            <ul v-if="currentStatus.status_id !== 0">-->
+<!--                                <li :key="status.id" v-for="status in statuses.filter(s=> !s.hide)"-->
+<!--                                    :class="{'text-ksaa': status.id <= currentStatus.status_id  && status.id < 6,'text-red-400':status.id===6,'items-center':true,'grid':true,'flex':true,'grid-cols-1':true}">-->
+<!--                                    <div class="flex justify-between items-center">-->
+<!--                                        <span v-if="status.id<6">-->
+<!--                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"-->
+<!--                                         fill="currentColor">-->
+<!--                                        <path fill-rule="evenodd"-->
+<!--                                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"-->
+<!--                                              clip-rule="evenodd"/>-->
+<!--                                    </svg>-->
+<!--                                    </span>-->
+<!--                                        <span v-else>-->
+<!--                                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"-->
+<!--                                           fill="currentColor">-->
+<!--  <path fill-rule="evenodd"-->
+<!--        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"-->
+<!--        clip-rule="evenodd"/>-->
+<!--</svg>-->
+<!--                                   </span>-->
+<!--                                        <div>-->
+<!--                                            {{status.title}}-->
+<!--                                        </div>-->
+<!--                                        <div>-->
+<!--                                            <svg @click="status.show_description=!status.show_description"-->
+<!--                                                 xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"-->
+<!--                                                 viewBox="0 0 24 24" stroke="currentColor">-->
+<!--                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
+<!--                                                      d="M19 9l-7 7-7-7"/>-->
+<!--                                            </svg>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
 
-                                    <div v-if="getstatusDiscriptions(status.id)!=={}">
-                                        <accept-detail v-if="status.id===2" :hide="status.show_description"
-                                                       :info="getstatusDiscriptions(status.id).detail"></accept-detail>
-                                        <exam-detail v-if="status.id===3"
-                                                     :info="getstatusDiscriptions(status.id).detail"></exam-detail>
-                                        <result-p-k v-if="status.id===4"
-                                                    :info="getstatusDiscriptions(status.id).detail"></result-p-k>
-                                    </div>
+<!--                                    <div v-if="getstatusDiscriptions(status.id)!=={}">-->
+<!--                                        <accept-detail v-if="status.id===2" :hide="status.show_description"-->
+<!--                                                       :info="getstatusDiscriptions(status.id).detail"></accept-detail>-->
+<!--                                        <exam-detail v-if="status.id===3"-->
+<!--                                                     :info="getstatusDiscriptions(status.id).detail"></exam-detail>-->
+<!--                                        <result-p-k v-if="status.id===4"-->
+<!--                                                    :info="getstatusDiscriptions(status.id).detail"></result-p-k>-->
+<!--                                    </div>-->
 
-                                </li>
-                            </ul>
-                            <div v-else><p>Процесс подачи заявления не начат. Если у Вас есть трудности, Вы можете обратится в службу поддержки
-                                <a href="tel:+7(4942)629-123">+7(4942)629-123</a> или отправить пакет документов на почту
-                                <a href="mailto:pk@ksaa.edu.ru">pk@ksaa.edu.ru</a> </p></div>
-                        </div>
+<!--                                </li>-->
+<!--                            </ul>-->
+<!--                            <div v-else><p>Процесс подачи заявления не начат. Если у Вас есть трудности, Вы можете обратится в службу поддержки-->
+<!--                                <a href="tel:+7(4942)629-123">+7(4942)629-123</a> или отправить пакет документов на почту-->
+<!--                                <a href="mailto:pk@ksaa.edu.ru">pk@ksaa.edu.ru</a> </p></div>-->
+<!--                        </div>-->
 
-                    </div>
+<!--                    </div>-->
                 </ion-grid>
             </div>
         </ion-content>
@@ -67,15 +68,18 @@
 
 <script>
     import {IonPage, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid} from '@ionic/vue';
-    import AcceptDetail from "@/components/AcceptDetail";
-    import ExamDetail from "@/components/ExamDetail";
-    import ResultPK from "@/components/ResultPK";
+    // import AcceptDetail from "@/components/AcceptDetail";
+    // import ExamDetail from "@/components/ExamDetail";
+    // import ResultPK from "@/components/ResultPK";
+    // import Steps from "@/components/Steps";
+    import Steps from "@/components/Steps";
 
     export default {
         name: 'Home',
         components: {
+            Steps,
             IonContent, IonPage, IonGrid,
-            AcceptDetail, ExamDetail, ResultPK
+            // AcceptDetail, ExamDetail, ResultPK
         },
         data() {
             return {
@@ -201,7 +205,8 @@
     }
 
     ion-content {
-        --background: linear-gradient(to bottom, #20b2aa 50%, transparent 50%);
+        /*--background: linear-gradient(to bottom, #20b2aa 20%, transparent 20%);*/
+        @apply rounded-b-2xl;
     }
 
 </style>
